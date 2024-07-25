b09f465eebce7cc922408f0bd2f981d34e2667df
mutshiv <vmutshinya@gmail.com>
2024-07-25 11:13:11 +0200

    Conflict introduction

--- FileTwo.java
+++ FileTwo.java
@@ -1,5 +1,5 @@
 public class FileTwo {
 	public static void main(String[] args) {
-		System.out.println("testing Git workflow");
+		System.out.println("testing Git workflow\nIntroducing a conflict!");
 	}
 }
